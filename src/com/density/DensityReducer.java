7dd6f5d720ea76312e9ec2d78b4fc3156e97dc80
hadoop user <William Yang>
2015-11-24 17:37:07 -0500

    Adjusted the algorithm for calculating the housing densities

--- src/com/density/DensityReducer.java
+++ src/com/density/DensityReducer.java
@@ -17,15 +17,18 @@ public class DensityReducer extends Reducer<Text, Text, Text, DoubleWritable> {
 		double population = 0;
 		double houses = 0;
 		double land = 0;
+		double count = 0;
+		double PWHD = 0;
 		
 		for(Text value : values){
 			densityData = value.toString();
 			dFields = densityData.split("\t", -1);
-			population += Double.parseDouble(dFields[0]);
-			houses += Double.parseDouble(dFields[1]);
-			land += Double.parseDouble(dFields[2]);
+			population = Double.parseDouble(dFields[0]);
+			houses = Double.parseDouble(dFields[1]);
+			land = Double.parseDouble(dFields[2]);
+			if(houses != 0 && population != 0 && land != 0) PWHD += (houses * land)/(population * 1000000);
+			count++; 
 		}
-		
-		context.write(key, new DoubleWritable((houses * land)/(1000000 * population)));
+		context.write(key, new DoubleWritable(PWHD/count));
 	}
 }
